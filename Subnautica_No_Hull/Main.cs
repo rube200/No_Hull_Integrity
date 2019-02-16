e5b07ed15e3b17403c556fdc7c857eefde0e7afc
rube200 <rube200@users.noreply.github.com>
2019-02-16 00:51:14 +0000

    Initial commit

---
+++ Subnautica_No_Hull/Main.cs
@@ -0,0 +1,25 @@
+﻿using Harmony;
+using System;
+using System.Reflection;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+namespace Subnautica_No_Hull
+{
+    public class Main
+    {
+        public static void Initialize()
+        {
+            HarmonyInstance.Create("Subnautica.Subnautica_No_Hull.mod").PatchAll(Assembly.GetExecutingAssembly());
+            DevConsole.disableConsole = false;
+            SceneManager.sceneLoaded += new UnityAction<Scene, LoadSceneMode>(OnSceneLoaded);
+            Console.WriteLine("[No Hull] Loaded");
+        }
+
+        private static void OnSceneLoaded(Scene Scene, LoadSceneMode mode)
+        {
+            if (Scene.name == "Aurora")
+                GameModeUtils.ActivateCheat(GameModeOption.NoPressure);
+        }
+    }
+}
---
+++ Subnautica_No_Hull/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Subnautica_No_Hull")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Subnautica_No_Hull")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0089d47f-c918-4ee8-afe5-b763b6d91bfc")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
