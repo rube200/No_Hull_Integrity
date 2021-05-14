31495a85615a5f5ec72256443c8e69b13858c754
Rúben Garcia <rube200@users.noreply.github.com>
2021-05-14 20:03:44 +0100

    Update plugin

--- Subnautica_No_Hull/Main.cs
+++ Subnautica_No_Hull/Main.cs
@@ -1,37 +1,32 @@
-﻿using Ryder;
-using System;
+﻿using System;
 using System.Reflection;
+using HarmonyLib;
 
-namespace Subnautica_No_Hull
+namespace RG.Subnautica_No_Hull
 {
     public class Main
     {
-        public static MethodRedirection RedirectAwake { get; private set; } = null;
-        public static MethodRedirection RedirectStart { get; private set; } = null;
+        public static readonly MethodInfo RequiresReinforcementsMethod =
+            typeof(GameModeUtils).GetMethod(nameof(GameModeUtils.RequiresReinforcements),
+                BindingFlags.Static | BindingFlags.Public);
 
+        public static readonly HarmonyMethod ReinforcementsCancelMethod =
+            new HarmonyMethod(typeof(Main).GetMethod(nameof(ReinforcementsCancel),
+                BindingFlags.Static | BindingFlags.NonPublic));
 
         public static void Initialize()
         {
-            var MethodAwake = typeof(BaseHullStrength).GetMethod("Awake", BindingFlags.Instance | BindingFlags.NonPublic);
-            var MethodStart = typeof(BaseHullStrength).GetMethod("Start", BindingFlags.Instance | BindingFlags.NonPublic);
-            var MethodRedirect = typeof(Main).GetMethod("Redirect", BindingFlags.Instance | BindingFlags.NonPublic);
-
-
-            if (RedirectAwake != null)
-                RedirectAwake.Dispose();
-            RedirectAwake = Redirection.Redirect(MethodAwake, MethodRedirect);
-
-
-            if (RedirectStart != null)
-                RedirectStart.Dispose();
-            RedirectStart = Redirection.Redirect(MethodStart, MethodRedirect);
-
-
+            var harmony = new Harmony("RG.Subnautica_No_Hull");
+            harmony.Patch(RequiresReinforcementsMethod, ReinforcementsCancelMethod);
             Console.WriteLine("[No Hull] Loaded");
         }
 
-#pragma warning disable IDE0051
-        private void Redirect() { }
-#pragma warning restore IDE0051
+        // ReSharper disable once InconsistentNaming
+        // ReSharper disable once RedundantAssignment
+        private static bool ReinforcementsCancel(ref bool __result)
+        {
+            __result = false;
+            return false;
+        }
     }
 }
